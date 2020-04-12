32e5330ab8a45f3be578388f3e4bace8e005cbec
number61971 <iamdamocles@gmail.com>
2020-04-12 16:10:05 -0500

    refactor: Move SlotMachine component into own directory in anticipation of organizing dependencies and subcomponents.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import SlotMachine from './SlotMachine';
+import SlotMachine from './slotMachine/SlotMachine';
 
 const useStyles = makeStyles((theme) => {
   return {
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import AppBar from '@material-ui/core/AppBar';
-import SlotMachine from './SlotMachine';
+import SlotMachine from './slotMachine/SlotMachine';
 
 let wrapper;
 
